3d49e7fbede39b1b75c3d9bfd7e4010b30bdaab2
Andrey <antonov.andrey.10101@gmail.com>
2023-09-07 17:27:36 +0300

    feat: add service

---
+++ src/components/services/GoogleAPI.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+interface IRequestParams {
+    search: string;
+    category: string;
+    orderBy: string;
+}
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const baseUrl = `https://www.googleapis.com/books/v1/volumes?q=subject:art&orderBy=newest&maxResults=12&key=${API_KEY}`;
+
+export const setQuery = ({ search, category, orderBy }: IRequestParams, currIndex = 0) => {
+  const cat = (category === 'all') ? null : `subject:${category}`;
+  return `${baseUrl}?q=${search}+${cat}&orderBy=${orderBy}&startIndex=${currIndex}&maxResults=30&key=${API_KEY}`;
+};
+
+export const initionLoad = async () => {
+  const res = await axios.get(baseUrl);
+  return res;
+};
+
+export const getBooks = async (args: IRequestParams, currIndex: number) => {
+  const res = await axios.get(setQuery(args, currIndex));
+  return res;
+};
+
+export const getBookDetails = async (id: string) => {
+  const res = await axios.get(`${baseUrl}/${id}`);
+  return res;
+};
