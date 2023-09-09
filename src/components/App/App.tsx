52cf1b836662389e59f57fad99099be08fb4a65d
Andrey <antonov.andrey.10101@gmail.com>
2023-09-09 09:52:42 +0300

    feat: add Page404

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from '../../pages/MainPage';
 import BookPage from '../../pages/BookPage';
 import './App.scss'
+import Page404 from '../../pages/Page404/Page404';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/:bookId" element={<BookPage />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
       </Router>
     </div>
